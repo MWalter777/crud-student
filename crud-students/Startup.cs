a8fb2618eb2a320d223452861572b9d6c2249493
MWalter777 <waltermarrom777@gmail.com>
2021-02-09 12:46:29 -0600

    first commit

---
+++ crud-students/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(crud_students.Startup))]
+namespace crud_students
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
