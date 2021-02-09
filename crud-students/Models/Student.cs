95b78b5b7a42f8f8fb008b192548547907db715f
MWalter777 <waltermarrom777@gmail.com>
2021-02-09 13:29:29 -0600

    Add connection to database

---
+++ crud-students/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿namespace crud_students.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
+    using System.Linq;
+
+    [Table("Student")]
+    public class Student
+    {
+        public Student()
+        {
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string surname { get; set; }
+
+        [Required]
+        public int age { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string gender { get; set; }
+
+        [Required]
+        public DateTime create_at { get; set; }
+
+    }
+}
---
+++ crud-students/Models/crud_studentsContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace crud_students.Models
+{
+    public class crud_studentsContext : DbContext
+    {  
+        public crud_studentsContext() : base("name=DefaultConnection") //base("name=crud_studentsContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<crud_students.Models.Student> Students { get; set; }
+    }
+}
