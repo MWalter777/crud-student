5a64947288f7f9592e32d1f1c29aa2ff105acc2b
Walter Marroquin <waltermarrom777@gmail.com>
2021-03-04 16:16:06 -0600

    Create Crud teacher

---
+++ crud-students/Controllers/RestTeachersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using crud_students.Models;
+
+namespace crud_students.Controllers
+{
+    public class RestTeachersController : ApiController
+    {
+        private Model db = new Model();
+
+        // GET: api/RestTeachers
+        public IQueryable<Teacher> GetTeachers()
+        {
+            return db.Teachers;
+        }
+
+        // GET: api/RestTeachers/5
+        [ResponseType(typeof(Teacher))]
+        public IHttpActionResult GetTeacher(int id)
+        {
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teacher);
+        }
+
+        // PUT: api/RestTeachers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTeacher(int id, Teacher teacher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != teacher.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(teacher).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeacherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/RestTeachers
+        [ResponseType(typeof(Teacher))]
+        public IHttpActionResult PostTeacher(Teacher teacher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Teachers.Add(teacher);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = teacher.id }, teacher);
+        }
+
+        // DELETE: api/RestTeachers/5
+        [ResponseType(typeof(Teacher))]
+        public IHttpActionResult DeleteTeacher(int id)
+        {
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            db.Teachers.Remove(teacher);
+            db.SaveChanges();
+
+            return Ok(teacher);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TeacherExists(int id)
+        {
+            return db.Teachers.Count(e => e.id == id) > 0;
+        }
+    }
+}
